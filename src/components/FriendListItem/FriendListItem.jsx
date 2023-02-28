7f090a64143f415add2550a39512c254250dc517
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-02-28 12:55:11 -0500

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,7 +1,6 @@
 import css from '../FriendListItem/FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
- // const status = isOnline;
   return (
     <li className={css.item}>
       <span className={isOnline ? css.online : css.offline}></span>
